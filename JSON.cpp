14d2ba37dfaffef17033020555f55b38e0e69308
Anfysa <104844855+Anfysa@users.noreply.github.com>
2023-05-06 13:00:46 +0300

    Update JSON.cpp

--- JSON.cpp
+++ JSON.cpp
@@ -1,20 +1,4 @@
-#include <string.h>
-#include "../rapidjson/include/rapidjson/document.h"
-
-void print(const char* msg);
-
-class JSONCommand
-{
-private:
-	rapidjson::Document command;
-	
-public:
-	JSONCommand(const char* str_json);
-	rapidjson::Document* getCommand();
-	std::string getCommandType();
-	double getAngle();
-	double getDistance();
-};
+#include "JSON.h"
 
 JSONCommand::JSONCommand(const char* str_json)
 {
