78648aabc8437a287c6aa9bc03cb7f8eea46cc3b
Anfysa <104844855+Anfysa@users.noreply.github.com>
2023-05-06 11:11:44 +0300

    Create Move.cpp

---
+++ Move.cpp
@@ -0,0 +1,107 @@
+#include "TCP.h"
+#include "GPIO.h"
+#include "JSON.h"
+
+class Robot
+{
+private:
+	TCPEntity* cmdrcv;
+	GPIOEntity* gpio_entity;
+	Motor* motorA;
+	Motor* motorB;
+	
+	JSONCommand* cmd;
+	
+	
+public:
+	Robot(TCPEntity* te, GPIOEntity* ge, Motor* ma, Motor* mb);
+	
+	int run();
+	
+	void forward(const double& val);
+	void backward(const double& val);
+	void turn_left(const double& val);
+	void turn_right(const double& val);
+};
+
+Robot::Robot(TCPEntity* te, GPIOEntity* ge, Motor* ma, Motor* mb) : cmdrcv(te), gpio_entity(ge), motorA(ma), motorB(mb)
+{
+	print("Подключено");
+}
+
+int Robot::run()
+{
+	int exit_code = 0;
+	
+	print("Жду команды");
+	exit_code += cmdrcv->Receive(cmd);
+	
+	if (exit_code == 0)
+	{
+		print("Выполняю команду");
+
+		if (cmd->getCommandType() == "movement")
+		{
+			std::string mt = cmd->getMoveType(); 
+			if (mt == "forward") forward(cmd->getDistance());
+			else if (mt == "backward") backward(cmd->getDistance());
+			else if (mt == "left") turn_left(cmd->getAngle());
+			else if (mt == "right") turn_right(cmd->getAngle());
+			else print("Не знаю такой команды");
+		}
+	}
+	else print("Что-то пошло не так...");
+	
+	char tmp_code = static_cast<char>('0' + exit_code);
+	cmdrcv->Send(&tmp_code);
+	return exit_code;
+}
+
+void Robot::forward(const double& val)
+{
+	print("Вперед");
+	motorA->run(0);
+	motorB->run(0);
+	usleep(static_cast<useconds_t>(1000*val));
+	motorA->stop();
+	motorB->stop();
+	
+	usleep(static_cast<useconds_t>(1000*200));
+}
+
+void Robot::backward(const double& val)
+{
+	print("Назад");
+	motorA->run(1);
+	motorB->run(1);
+	usleep(static_cast<useconds_t>(1000*val));
+	motorA->stop();
+	motorB->stop();
+	
+	usleep(static_cast<useconds_t>(1000*200));
+}
+
+void Robot::turn_left(const double& val)
+{
+	print("Налево");
+	motorA->run(0);
+	motorB->run(1);
+	usleep(static_cast<useconds_t>(1000*val));
+	motorA->stop();
+	motorB->stop();
+	
+	usleep(static_cast<useconds_t>(1000*200));
+}
+
+void Robot::turn_right(const double& val)
+{
+	print("Направо");
+	motorA->run(1);
+	motorB->run(0);
+	usleep(static_cast<useconds_t>(1000*val));
+	motorA->stop();
+	motorB->stop();
+	
+	usleep(static_cast<useconds_t>(1000*200));
+}
+
